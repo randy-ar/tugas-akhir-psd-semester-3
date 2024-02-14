bd5beee4b2ed1ec62b4d9eafed7f5d11781858c9
randy-ar <randyabdulrahman87@gmail.com>
2024-02-14 19:54:13 +0700

    push

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,17 +10,17 @@ from sklearn.ensemble import RandomForestRegressor
 st.header(Path(__file__).resolve())
 st.header(Path(__file__).resolve().parent)
 
-# df = pd.read_csv(str(Path(__file__).resolve().parent)+"/data_hour.csv") # Membaca data set hour.csv
-# # Menganti nama kolom
-# df = df.rename(columns={'weathersit':'weather',
-#                        'yr':'year',
-#                        'mnth':'month',
-#                        'hr':'hour',
-#                        'hum':'humidity',
-#                        'cnt':'count'})
-# df = df.drop(columns = ['instant' , 'dteday' , 'year']) # hapus kolom yang tidak diperlukan
-# # Membuat list kolom
-# cols = ['season' , 'month' , 'hour' , 'holiday' , 'weekday' , 'workingday' , 'weather']
-# # Mengganti Dtype kolom menjadi Dtype Category
-# for col in cols:
-#     df[col] = df[col].astype('category')
+df = pd.read_csv(str(Path(__file__).resolve().parent)+"/hour.csv") # Membaca data set hour.csv
+# Menganti nama kolom
+df = df.rename(columns={'weathersit':'weather',
+                       'yr':'year',
+                       'mnth':'month',
+                       'hr':'hour',
+                       'hum':'humidity',
+                       'cnt':'count'})
+df = df.drop(columns = ['instant' , 'dteday' , 'year']) # hapus kolom yang tidak diperlukan
+# Membuat list kolom
+cols = ['season' , 'month' , 'hour' , 'holiday' , 'weekday' , 'workingday' , 'weather']
+# Mengganti Dtype kolom menjadi Dtype Category
+for col in cols:
+    df[col] = df[col].astype('category')
