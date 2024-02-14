196a87aee8705cc9f48fe5baef14e9da5a089ad8
randy-ar <randyabdulrahman87@gmail.com>
2024-02-14 20:18:31 +0700

    pudh

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,7 +8,7 @@ import joblib
 from sklearn.ensemble import RandomForestRegressor
 
 # st.header(Path(__file__).resolve())
-st.header("Prediksi Jumlah Kebutuhan Peminjaman Sepeda Menggunakan Methode Regresi")
+st.title("Prediksi Jumlah Kebutuhan Peminjaman Sepeda Menggunakan Methode Regresi")
 
 df = pd.read_csv(str(Path(__file__).resolve().parent)+"/hour.csv") # Membaca data set hour.csv
 # Menganti nama kolom
@@ -26,18 +26,30 @@ for col in cols:
     df[col] = df[col].astype('category')
 
 
-col1, col2 = st.columns(2)
 
-col1.title("Pengaruh Penggunaaan Sepeda Selama Hari Kerja dan Hari Libur")
+st.text("Pengaruh Penggunaaan Sepeda Selama Hari Kerja dan Hari Libur")
 # Melihat pengaruh penggunaaan sepeda selama hari kerja dan hari libur
 fig, ax = plt.subplots(figsize=(20,5))
 sns.pointplot(data=df, x='hour', y='count', hue='weekday', ax=ax)
 ax.set(title='Jumlah sepeda selama weekdays dan weekends')
-col1.pyplot(fig)
+st.pyplot(fig)
+st.download_button("Download chart", fig)
 
-col2.title("Pengaruh Penggunaaan Sepeda Selama Hari Kerja dan Hari Libur")
+st.text("Pengaruh Penggunaaan Sepeda Terdaftar Selama Hari Kerja dan Hari Libur")
 # Melihat pengaruh penggunaaan sepeda selama hari kerja dan hari libur pada pengguna yang tidak terdaftar
 fig, ax = plt.subplots(figsize=(20,5))
 sns.pointplot(data=df, x='hour', y='casual', hue='weekday', ax=ax)
 ax.set(title='Jumlah sepeda selama weekdays dan weekends pada pengguna yang tidak terdaftar')
-col2.pyplot(fig)
+st.pyplot(fig)
+st.download_button("Download chart", fig)
+
+
+st.text("Pengaruh Penggunaaan Sepeda Tidak Terdaftar Selama Hari Kerja dan Hari Libur")
+# Melihat pengaruh penggunaaan sepeda selama hari kerja dan hari libur pada pengguna yang terdaftar
+fig, ax = plt.subplots(figsize=(20,5))
+sns.pointplot(data=df, x='hour', y='registered', hue='weekday', ax=ax)
+ax.set(title='Jumlah sepeda selama weekdays dan weekends pada pengguna yang tidak terdaftar')
+st.pyplot(fig)
+st.download_button("Download chart", fig)
+
+
