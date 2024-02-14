31b0cab1bc0877c9ffd74eb99e8441fce3618c69
randy-ar <randyabdulrahman87@gmail.com>
2024-02-14 20:33:26 +0700

    push

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -33,7 +33,7 @@ fig, ax = plt.subplots(figsize=(20,5))
 sns.pointplot(data=df, x='hour', y='count', hue='weekday', ax=ax)
 ax.set(title='Jumlah sepeda selama weekdays dan weekends')
 st.pyplot(fig)
-st.download_button("Download chart", fig)
+st.code("Dari grafik ini kita dapat lihat jika penggunaan sepeda dominan ada dihari kerja, tepatnya pada jam berangkat kerja dan pulang kerja. Informasi ini dapat digunakan untuk memprediksi kenaikan kebutuhan peminjaman sepeda pada jam berangkat kerja dan pulang kerja.")
 
 st.text("Pengaruh Penggunaaan Sepeda Terdaftar Selama Hari Kerja dan Hari Libur")
 # Melihat pengaruh penggunaaan sepeda selama hari kerja dan hari libur pada pengguna yang tidak terdaftar
@@ -41,8 +41,7 @@ fig, ax = plt.subplots(figsize=(20,5))
 sns.pointplot(data=df, x='hour', y='casual', hue='weekday', ax=ax)
 ax.set(title='Jumlah sepeda selama weekdays dan weekends pada pengguna yang tidak terdaftar')
 st.pyplot(fig)
-st.download_button("Download chart", fig)
-
+st.code("Dari grafik ini kita dapat lihat jika penggunaan sepeda yang tidak terdaftar dominan ada dihari libur. Informasi ini dapat digunakan untuk memprediksi kenaikan kebutuhan peminjaman pengguna yang tidak terdaftar pada hari libur dan penurunan kebutuhan peminjaman sepeda pada pengguna yang tidak terdaftar.")
 
 st.text("Pengaruh Penggunaaan Sepeda Tidak Terdaftar Selama Hari Kerja dan Hari Libur")
 # Melihat pengaruh penggunaaan sepeda selama hari kerja dan hari libur pada pengguna yang terdaftar
@@ -50,6 +49,11 @@ fig, ax = plt.subplots(figsize=(20,5))
 sns.pointplot(data=df, x='hour', y='registered', hue='weekday', ax=ax)
 ax.set(title='Jumlah sepeda selama weekdays dan weekends pada pengguna yang tidak terdaftar')
 st.pyplot(fig)
-st.download_button("Download chart", fig)
+st.code("Dari grafik ini kita dapat lihat jika penggunaan sepeda yang terdaftar dominan ada dihari kerja. Informasi ini dapat digunakan untuk memprediksi kenaikan dan penurunan kebutuhan peminjaman sepeda pada pengguna yang terdaftar.")
 
-
+st.text("Pengaruh Penggunaaan Sepeda Pada Setiap Kondisi Cuaca")
+# Melihat pengaruh penggunaaan sepeda pada setiap cuaca
+fig, ax = plt.subplots(figsize=(20,5))
+sns.pointplot(data=df, x='hour', y='count', hue='weather', ax=ax)
+ax.set(title='Jumlah sepeda pada setiap cuaca')
+st.code("Dari grafik ini kita dapat lihat jika tren penggunaan sepeda menurun cukup banyak pada musim dingin. Informasi ini dapat digunakan untuk memprediksi penurunan tren peminjaman sepeda pada musim dingin.")
